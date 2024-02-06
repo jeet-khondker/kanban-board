a3a62404d44a6487064aabb1d5e817c120c1c1f8
Jeet Khondker <jeetzhkhondker@gmail.com>
2024-02-06 16:23:28 +0900

    feature : Created Functionality for Updating Tasks Count in real time

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ const allowdrop = (event) => {
 const drag = (event) => {
     event.dataTransfer.setData("text", event.target.id);
 }
+
+
+// Function : Update Task(s) Count
+const updateTasksCount = () => {
+    const backlogColumn = document.getElementById("backlog");
+    const readyColumn = document.getElementById("ready");
+    const inProgressColumn = document.getElementById("in-progress");
+    const inReviewedColumn = document.getElementById("in-review");
+
+    const backlogHeader = document.getElementById("backlog-header");
+    const readyHeader = document.getElementById("ready-header");
+    const inProgressHeader = document.getElementById("progress-header");
+    const inReviewedHeader = document.getElementById("review-header");
+
+    backlogHeader.innerText = `${ backlogHeader.innerText.split(" ")[0]}${ backlogColumn.children.length } ${ backlogColumn.children.length > 1 ? "Tasks" : "Task" }`;
+}
+
+updateTasksCount();
